0b3e2933ef0f4071719699e838f68660817bfa72
test5574663-coder <test5574663@gmail.com>
2026-02-24 08:49:43 +0700

    Change bot token in deploy-commands.js
    Updated the bot token to a new value.

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@
 const { REST, Routes, SlashCommandBuilder } = require("discord.js");
 const TOKEN = process.env.TOKEN;
 
-const TOKEN = "MTQ3MjE2MTkzNDYwOTc0NDEwNg.GKIbbm.eylpi9BfDyJVYRR7ErTOz-Xz9emZcUlrkHc08A";
+const TOKEN = "MTQ3MjE2MTkzNDYwOTc0NDEwNg.Gvjyt_.KsciJrWATk2a5_SOjaUbH98fATQsek2HKkUvGQ";
 const CLIENT_ID = "1472161934609744106";
 const GUILD_ID = "1466476014908473550";
 
